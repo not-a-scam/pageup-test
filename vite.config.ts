dd4eea37b14e5913419c257264a92f3b623938d6
Zechariah Yeo <70231898+not-a-scam@users.noreply.github.com>
2026-02-13 00:41:35 +0800

    Update path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/shop/",
+  base: "/pageup-test/",
   server: {
     host: "::",
     port: 8080,
